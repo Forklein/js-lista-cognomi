2810a39637178c173a6cf2005986cb0068bbc0d3
Forklein <giuseppepisani2@gmail.com>
2021-07-06 13:30:38 +0200

    Set variables and for cicle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 console.log('Ciao Peppe');
+
+
+/* Consegna:
+1. chiedi all’utente il cognome
+2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+3. stampa la lista ordinata alfabeticamente sia in console che in pagina
+4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+*/
+
+
+var userSurname = prompt('Inserisci il tuo cognome').trim();
+var listSurname = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+listSurname.push(userSurname);
+console.log(listSurname);
+listSurname.sort();
+console.log(listSurname);
+console.log(listSurname.length);
+
+var isPosition = false;
+
+for (var i = 0; i < listSurname.length; i++) {
+    if ()
+}
